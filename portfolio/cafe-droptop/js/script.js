63494404771bccc48e94e2f495e14d17a8f4222e
pic22ti <65947265+pic22ti@users.noreply.github.com>
2020-07-14 17:53:54 +0900

    update

--- portfolio/cafe-droptop/js/script.js
+++ portfolio/cafe-droptop/js/script.js
@@ -1,9 +1,41 @@
-let mainImg = document.querySelector('.main-img');
-let mainDot = document.querySelector('.main>.flex');
+// // modal
+// const imgItem = document.querySelector('.newitem>.flex')
+// const modalTag = document.querySelector('.modal');
+// const closeBtn = modalTag.children[0];
 
-mainDot.addEventListener('click', function(e){
-  let target = e.target;
-  let dotClass = target.getAttribute('class');
-  mainImg.style.backgroundImage = `url('../images/${dotClass}.jpg')`;
-});
+// imgItem.addEventListener('click', function(e){
+//   let target = e.target;
+//   let src = target.getAttribute('src');
+//   if( src === null ) {
+//     return;
+//   }
+//   modalTag.style.display = "block";
+//   modalTag.children[1].src = src;
+// });
 
+// closeBtn.addEventListener('click', function(e){
+//   modalTag.style.display = "none";
+// });
+
+
+
+
+
+
+// button
+let btnMenu = document.querySelector('.btn>.flex');
+let btnMenuChild = btnMenu.children;
+let showMenu = document.querySelectorAll('ul.content');
+let i;
+let preTarget = null;
+
+// for문 사용
+for( i=0; i<btnMenuChild.length; i++ ){    
+  btnMenuChild[i].addEventListener('click', function(e){
+    let target = e.target;
+    for( let j=0 ; j<btnMenuChild.length ; j++ ){
+      btnMenuChild[j].classList.remove('click');
+    }
+    this.classList.add('click');    
+  });
+}
