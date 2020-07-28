3f6889a19913939cb9ee45cda0d388f8a8965e54
pic22ti <65947265+pic22ti@users.noreply.github.com>
2020-07-28 15:22:24 +0900

    Update script-coffee.js

--- portfolio/cafe-droptop/js/script-coffee.js
+++ portfolio/cafe-droptop/js/script-coffee.js
@@ -27,7 +27,7 @@ function goToTop() {
 window.onscroll = function(e) {
 
   /* 각 값이 스크롤 위에 출력되게 해서 확인하기 */
-  document.querySelector('#scrolltest').textContent = window.outerHeight+document.documentElement.scrollTop+">="+document.body.offsetHeight;
+  document.querySelector('#scrolltest').textContent = window.outerHeight+document.documentElement.scrollTop+"outer>="+document.body.offsetHeight;
 
   
   // 스크롤이 화면 맨 밑에 있다면
